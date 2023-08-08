52ffe38e8192b40027a311062801913f5f1e8ee7
malcq <malcqq@gmail.com>
2023-08-09 01:30:36 +0300

    install react, react-dom, add cssLoader, create Counter,

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { createRoot } from 'react-dom/client';
+import { Counter } from './components/Counter';
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+  <div>
+    <Counter />
+  </div>
+);
