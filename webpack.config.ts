cf57fa86d2c59aef0fa772191e66b4dd1db638c9
malcq <malcqq@gmail.com>
2023-09-07 07:27:42 +0300

    zxc

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,6 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
-import { buildWebpackConfig } from './config/build/buildwebpackconfig';
+import { buildWebpackConfig } from './config/build/buildWebpackConfig';
 import { BuildEnv, BuildPaths } from './config/build/types/config';
 
 export default (env: BuildEnv) => {
