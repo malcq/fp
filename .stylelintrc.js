0b6894517bc4c7e83d83a39ddbea8f2dce6c8ac8
malcq <malcqq@gmail.com>
2023-09-07 06:06:22 +0300

    remove stylelint-rational-order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,24 +1,12 @@
 module.exports = {
   extends: [
     'stylelint-config-standard-scss',
-    'stylelint-config-idiomatic-order',
-    'stylelint-config-rational-order'
+    'stylelint-config-idiomatic-order'
   ],
-  plugins: [
-    'stylelint-scss',
-    'stylelint-order',
-    'stylelint-config-rational-order/plugin'
-  ],
+  plugins: ['stylelint-scss', 'stylelint-order'],
   rules: {
     'selector-class-pattern': null,
     'order/properties-order': [],
-    'plugin/rational-order': [
-      true,
-      {
-        'border-in-box-model': false,
-        'empty-line-between-groups': true
-      }
-    ],
     'order/order': ['custom-properties', 'declarations'],
     'order/properties-order': ['width', 'height'],
     'declaration-empty-line-before': [
